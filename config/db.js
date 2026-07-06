d40d56ad7af4cfc20efba31fa022a7707d764f8a
RoushanSri <roshansrivastav59796@gmail.com>
2026-07-06 17:43:46 +0530

    feat: add database configuration and Docker support for project deployment

--- config/db.js
+++ config/db.js
@@ -4,12 +4,6 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // Required for most hosted PostgreSQL providers
-    },
-  },
   logging: false,
 });
 
