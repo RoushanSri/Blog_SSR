d6d97461a5de86166032d39a247278c79b4894df
RoushanSri <roshansrivastav59796@gmail.com>
2026-07-07 15:18:21 +0530

    Role Based Access Fixed

--- middlewares/injectUser.js
+++ middlewares/injectUser.js
@@ -2,29 +2,31 @@ import jwt from "jsonwebtoken";
 import { getMeService } from "../services/user.service.js";
 
 const injectUser = async (req, res, next) => {
-    try {
-        const token = req.cookies?.accessToken;
-        if (!token) {
-            res.locals.user = null;
-            return next();
-        }
-
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-        const userData = await getMeService(decodedToken.id);
-
-        if (userData.success) {
-            res.locals.user = userData.data;
-            res.locals.roles = userData.data.roles.filter(role => role.isApproved == true ? role.roleName : null);
-        } else {
-            res.locals.user = null;
-            res.locals.roles = [];
-        }
-
-        next();
-    } catch (error) {
-        res.locals.user = null;
-        next();
+  try {
+    const token = req.cookies?.accessToken;
+    if (!token) {
+      res.locals.user = null;
+      return next();
     }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const userData = await getMeService(decodedToken.id);
+
+    if (userData.success) {
+      res.locals.user = userData.data;
+      res.locals.roles = userData.data.roles
+        .filter((role) => role.isApproved)
+        .map((role) => role.roleName);
+    } else {
+      res.locals.user = null;
+      res.locals.roles = [];
+    }
+
+    next();
+  } catch (error) {
+    res.locals.user = null;
+    next();
+  }
 };
 
 export default injectUser;
